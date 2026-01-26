0c3a2e06263317c5a2d8e2433b1578f730231784
massimodamico86-art <massimodamico86@gmail.com>
2026-01-25 21:39:36 -0500

    feat(17-01): update template components barrel exports
    - Add FeaturedTemplatesRow, TemplateGrid, TemplateCard exports
    - Add TemplateSidebar export
    - Organize exports by feature: marketplace, preview, modal

--- src/components/templates/index.js
+++ src/components/templates/index.js
@@ -4,7 +4,15 @@
  * Components for the Templates page UX improvements.
  */
 
+// Marketplace components
+export { FeaturedTemplatesRow } from './FeaturedTemplatesRow';
+export { TemplateGrid, TemplateCard } from './TemplateGrid';
+export { TemplateSidebar } from './TemplateSidebar';
+
+// Preview components
 export { TemplatePreviewPopover, useTemplatePreview } from './TemplatePreviewPopover';
 export { TemplateLivePreview, TemplateIframePreview } from './TemplateLivePreview';
+
+// Modal components
 export { TemplateCustomizeModal } from './TemplateCustomizeModal';
 export { SaveAsTemplateModal } from './SaveAsTemplateModal';
